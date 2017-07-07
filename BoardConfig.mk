ab9dd34954538adc9a8a2ca80f92d0ea84483b7f
Martinusbe <martinusbe@gmail.com>
2017-07-07 09:32:02 +0200

    raphael: adapt the exact values for lockscreen charging info
    real time values
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,9 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
 endif
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msmnile
