363668af8b22719fb35484b8a644d60c997713fd
Raaj52 <mail2raaj52@gmail.com>
2019-09-20 19:07:05 +0300

    raphael: Let's Derp-ify! * Added Derp stuffs! * Removed Live Display * Added ANXCamera support * Added README

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_raphael.mk
+    $(LOCAL_DIR)/derp_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_raphael-user \
+    derp_raphael-userdebug \
+    derp_raphael-eng
---
+++ derp_raphael.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Official-ify
+DERP_BUILDTYPE := Official
+
+# Setup Gapps options
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_MINIMAL_APPS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := raphael
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := derp_raphael
+
+BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+$(call inherit-product, vendor/ANXCamera/config.mk)
+
