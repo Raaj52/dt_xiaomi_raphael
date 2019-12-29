gr0ndpa/android_device_xiaomi_raphael

raphael: parts: Move dc dimming to Settings > Display

parts/src/org/lineageos/settings/display/DcDimmingSettingsActivity.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.display;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class DcDimmingSettingsActivity extends PreferenceActivity {
+
+    private static final String TAG_DCDIMMING = "dcdimming";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new DcDimmingSettingsFragment(), TAG_DCDIMMING).commit();
+    }
+}

parts/src/org/lineageos/settings/display/DcDimmingSettingsFragment.java
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.display;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import org.lineageos.settings.R;
+
+import vendor.xiaomi.hardware.displayfeature.V1_0.IDisplayFeature;
+
+public class DcDimmingSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private SwitchPreference mDcDimmingPreference;
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private IDisplayFeature mDisplayFeature;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dcdimming_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        try {
+            mDisplayFeature = IDisplayFeature.getService();
+        } catch(Exception e) {
+        }
+        mDcDimmingPreference = (SwitchPreference) findPreference(DC_DIMMING_ENABLE_KEY);
+        mDcDimmingPreference.setEnabled(true);
+        mDcDimmingPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (DC_DIMMING_ENABLE_KEY.equals(preference.getKey())) {
+            enableDcDimming((Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+    private void enableDcDimming(int enable) {
+        if (mDisplayFeature == null) return;
+        try {
+            mDisplayFeature.setFeature(0, 20, enable, 255);
+        } catch(Exception e) {
+        }
+    }
+}

Raaj52/dt_xiaomi_raphael

parts/src/org/chaldeastudio/deviceparts/display/DcDimmingSettingsActivity.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.chaldeastudio.deviceparts.display;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class DcDimmingSettingsActivity extends PreferenceActivity {
+
+    private static final String TAG_DCDIMMING = "dcdimming";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new DcDimmingSettingsFragment(), TAG_DCDIMMING).commit();
+    }
+}

parts/src/org/chaldeastudio/deviceparts/display/DcDimmingSettingsFragment.java
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.chaldeastudio.deviceparts.display;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import org.chaldeastudio.deviceparts.R;
+
+import vendor.xiaomi.hardware.displayfeature.V1_0.IDisplayFeature;
+
+public class DcDimmingSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private SwitchPreference mDcDimmingPreference;
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private IDisplayFeature mDisplayFeature;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dcdimming_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        try {
+            mDisplayFeature = IDisplayFeature.getService();
+        } catch(Exception e) {
+        }
+        mDcDimmingPreference = (SwitchPreference) findPreference(DC_DIMMING_ENABLE_KEY);
+        mDcDimmingPreference.setEnabled(true);
+        mDcDimmingPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (DC_DIMMING_ENABLE_KEY.equals(preference.getKey())) {
+            enableDcDimming((Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+    private void enableDcDimming(int enable) {
+        if (mDisplayFeature == null) return;
+        try {
+            mDisplayFeature.setFeature(0, 20, enable, 255);
+        } catch(Exception e) {
+        }
+    }
+}