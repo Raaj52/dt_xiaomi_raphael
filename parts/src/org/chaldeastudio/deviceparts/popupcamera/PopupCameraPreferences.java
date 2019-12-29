gr0ndpa/android_device_xiaomi_raphael

raphael: parts: Add settings for popup camera

parts/src/org/lineageos/settings/popupcamera/PopupCameraPreferences.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.popupcamera;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PopupCameraPreferences {
+
+    private static final String TAG = "PopupCameraUtils";
+    private static final boolean DEBUG = false;
+    private SharedPreferences mSharedPrefs;
+
+    private static final String LED_EFFECT_KEY = "popup_led_effect";
+    private static final boolean LED_EFFECT_DEFAULT_VALUE = true;
+
+    private static final String SOUND_EFFECT_KEY = "popup_sound_effect";
+    private static final String SOUND_EFFECT_DEFAULT_VALUE = "muqin";
+
+    public PopupCameraPreferences(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public String getSoundEffect() {
+        return mSharedPrefs.getString(SOUND_EFFECT_KEY, SOUND_EFFECT_DEFAULT_VALUE);
+    }
+
+    public boolean isLedAllowed() {
+        return mSharedPrefs.getBoolean(LED_EFFECT_KEY, LED_EFFECT_DEFAULT_VALUE);
+    }
+}

parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.popupcamera;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PopupCameraSettingsActivity extends PreferenceActivity {
+
+    private static final String TAG_POPUPCAMERA = "popupcamera";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PopupCameraSettingsFragment(), TAG_POPUPCAMERA).commit();
+    }
+}

parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsFragment.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.popupcamera;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+
+import org.lineageos.settings.R;
+
+public class PopupCameraSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.popup_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}

Raaj52/dt_xiaomi_raphael

parts/src/org/chaldeastudio/deviceparts/popupcamera/PopupCameraPreferences.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.chaldeastudio.deviceparts.popupcamera;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PopupCameraPreferences {
+
+    private static final String TAG = "PopupCameraUtils";
+    private static final boolean DEBUG = false;
+    private SharedPreferences mSharedPrefs;
+
+    private static final String LED_EFFECT_KEY = "popup_led_effect";
+    private static final boolean LED_EFFECT_DEFAULT_VALUE = true;
+
+    private static final String SOUND_EFFECT_KEY = "popup_sound_effect";
+    private static final String SOUND_EFFECT_DEFAULT_VALUE = "muqin";
+
+    public PopupCameraPreferences(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public String getSoundEffect() {
+        return mSharedPrefs.getString(SOUND_EFFECT_KEY, SOUND_EFFECT_DEFAULT_VALUE);
+    }
+
+    public boolean isLedAllowed() {
+        return mSharedPrefs.getBoolean(LED_EFFECT_KEY, LED_EFFECT_DEFAULT_VALUE);
+    }
+}

parts/src/org/chaldeastudio/deviceparts/popupcamera/PopupCameraSettingsActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.chaldeastudio.deviceparts.popupcamera;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PopupCameraSettingsActivity extends PreferenceActivity {
+
+    private static final String TAG_POPUPCAMERA = "popupcamera";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PopupCameraSettingsFragment(), TAG_POPUPCAMERA).commit();
+    }
+}

parts/src/org/chaldeastudio/deviceparts/popupcamera/PopupCameraSettingsFragment.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.chaldeastudio.deviceparts.popupcamera;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+
+import org.chaldeastudio.deviceparts.R;
+
+public class PopupCameraSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.popup_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}