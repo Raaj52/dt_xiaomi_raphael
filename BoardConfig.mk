3dcb8fdd197cd28d7501526cc795af09d2fec7d8
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-02 09:32:01 +0900

    raphael: build kernel with the latest clang possible
    * I don't know if there's specific predefined variable
      of it, so do oldskool ways instead ¯\_(ツ)_/¯
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ TARGET_KERNEL_ARCH := arm64
 ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := raphael_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
+  TARGET_KERNEL_CLANG_VERSION := $(shell sh -c "find prebuilts/clang/host/linux-x86/ -type f -name clang -printf \"%T@ %Tc %p\n\" | sort -n | tail -n1 | sed 's/.*clang-\(.*\)\/bin.*/\1/'")
   TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
 endif
 
