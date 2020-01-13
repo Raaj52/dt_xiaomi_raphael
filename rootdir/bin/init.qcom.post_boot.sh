ab3f6e03313d64b0e75123db74e42b06b323156b
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-13 18:55:11 +0900

    raphael: post_boot: use Pixel 4 cpusets config
    And also remove cpuset setup from init.target.rc
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -131,9 +131,13 @@ case "$target" in
     echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
     # cpuset parameters
+    echo 1-2 > /dev/cpuset/audio-app/cpus
     echo 0-1 > /dev/cpuset/background/cpus
-    echo 0-2 > /dev/cpuset/system-background/cpus
+    echo 0-7 > /dev/cpuset/camera-daemon/cpus
+    echo 0-3,5-6 > /dev/cpuset/foreground/cpus
     echo 0-3 > /dev/cpuset/restricted/cpus
+    echo 0-3 > /dev/cpuset/system-background/cpus
+    echo 0-7 > /dev/cpuset/top-app/cpus
 
     # Setup final blkio
     # value for group_idle is us
