d029e270e26a898da43daf8902a4417f71ef3c57
Nanda Oktavera <codeharuka.yusa@gmail.com>
2020-01-28 18:11:16 +0900

    raphael: post_boot: reconfigure zram creation
    DrunkAuroraForum
    
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -361,28 +361,21 @@ function configure_zram_parameters() {
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
     MemTotal=${MemTotalStr:16:8}
 
-    low_ram=`getprop ro.config.low_ram`
-
     # Zram disk - 75% for Go devices.
     # For 512MB Go device, size = 384MB, set same for Non-Go.
     # For 1GB Go device, size = 768MB, set same for Non-Go.
-    # For >=2GB Non-Go devices, size = 50% of RAM size. Limit the size to 4GB.
+    # For >=2GB Non-Go devices, size = 20% of RAM size. Limit the size to 1GB.
     # And enable lz4 zram compression for Go targets.
 
     RamSizeGB=`echo "($MemTotal / 1048576 ) + 1" | bc`
-    zRamSizeBytes=`echo "$RamSizeGB * 1024 * 1024 * 1024 / 2" | bc`
-    if [ $zRamSizeBytes -gt 4294967296 ]; then
-        zRamSizeBytes=4294967296
+    zRamSizeBytes=`echo "$RamSizeGB * 1024 * 1024 * 1024 / 4" | bc`
+    if [ $zRamSizeBytes -gt 1073741824 ]; then
+        zRamSizeBytes=1073741824
     fi
 
-    if [ "$low_ram" == "true" ]; then
-        echo lz4 > /sys/block/zram0/comp_algorithm
-    fi
+    echo lz4 > /sys/block/zram0/comp_algorithm
 
     if [ -f /sys/block/zram0/disksize ]; then
-        if [ -f /sys/block/zram0/use_dedup ]; then
-            echo 1 > /sys/block/zram0/use_dedup
-        fi
         if [ $MemTotal -le 524288 ]; then
             echo 402653184 > /sys/block/zram0/disksize
         elif [ $MemTotal -le 1048576 ]; then
@@ -391,7 +384,7 @@ function configure_zram_parameters() {
             echo $zRamSizeBytes > /sys/block/zram0/disksize
         fi
         mkswap /dev/block/zram0
-        swapon /dev/block/zram0 -p 32758
+        swapon -d -p 32758 /dev/block/zram0
     fi
 }
 
