8bb750f9de54cfe7571183b311de964aa2e4155d
Nauval Rizky <enuma.alrizky@gmail.com>
2020-02-20 12:09:49 +0900

    raphael: fod: read size and offset from properties
    * Let it configurable from (vendor) properties instead
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -57,6 +57,10 @@ public:
 private:
     sp<IDisplayFeature> xiaomiDisplayFeatureService;
     sp<IXiaomiFingerprint> xiaomiFingerprintService;
+
+    // android system properties
+    static constexpr const char* propFODOffset = "persist.vendor.sys.fp.fod.location.X_Y";
+    static constexpr const char* propFODSize = "persist.vendor.sys.fp.fod.size.width_height";
 };
 
 }  // namespace implementation
