c3870a91755fb464a4af076bb6456942a25b9c7e
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-29 18:03:53 +0900

    raphael: Revise device identity properties
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- derp_raphael.mk
+++ derp_raphael.mk
@@ -29,11 +29,14 @@ TARGET_MINIMAL_APPS := false
 PRODUCT_NAME := derp_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 9T Pro
+PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="raphael" \
-    TARGET_DEVICE="raphael"
+    TARGET_DEVICE="raphael" \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.021 4565141 release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
